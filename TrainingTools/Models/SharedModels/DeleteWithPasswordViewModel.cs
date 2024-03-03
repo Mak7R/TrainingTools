0c377271a1c37ce7c62303750e048b0fbef5f8d2
Mak7R <maksymrudakov@gmail.com>
2024-03-03 21:58:00 +0200

    Installed bootstrap. Fixed change password modal.

--- TrainingTools/Models/SharedModels/DeleteWithPasswordViewModel.cs
+++ TrainingTools/Models/SharedModels/DeleteWithPasswordViewModel.cs
@@ -5,6 +5,10 @@ public class DeleteWithPasswordViewModel
     public string ButtonName { get; set; }
     public string Name { get; set; }
     public string HandlerUrl { get; set; }
+    
+    /// <summary>
+    /// Write: bsDeleteModal.hide(); to hide modal
+    /// </summary>
     public string OnSuccess { get; set; }
 
 
