4f59bc412be68cd780ea74904bec3eb713d11ae7
Mak7R <maksymrudakov@gmail.com>
2024-07-10 17:47:45 +0300

    Infrastructure fixes.

--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -83,7 +83,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
